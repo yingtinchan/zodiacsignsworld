59e7f8c608b6fa950f4c8c52037c30c6c19d8336
yingtinchan <38816573+yingtinchan@users.noreply.github.com>
2023-11-23 11:15:19 +0800

    Delete test file directory

--- test file/test file/Test_2021_12_07/script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Documentfunction showTime() 
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();    /* set function: the */
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
